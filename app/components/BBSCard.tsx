318ed0e0b1a10c44fa76a7a5ac9e11a924c453eb
OS <ogisyou1249605@gmail.com>
2024-09-25 06:05:22 +0900

    Next.jsの初期設定

---
+++ app/components/BBSCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Link from 'next/link';
+
+const BBSCard = () => {
+  return (
+    <Card >
+    <CardHeader>
+      <CardTitle>Create project</CardTitle>
+      <CardDescription>
+        Deploy your new project in one-click.
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      Here you can start your new project effortlessly. Click "Read more" to
+      explore the steps and details about how to begin.
+    </CardContent>
+    <CardFooter className="flex justify-between">
+      <Link href="/" className="text-blue-500 hover:underline">
+        Read more
+      </Link>
+    </CardFooter>
+  </Card>
+  )
+}
+
+export default BBSCard
